e6e77c586fbf7d8b5a1bd1fddf30be8341f5f2c7
DanielaBarrios <103336142+DanielaBarrios@users.noreply.github.com>
2022-04-10 02:25:44 -0500

    Update practica2.py

--- practica2.py
+++ practica2.py
@@ -3,116 +3,282 @@ from matplotlib import pyplot as plt
 import math as ma
 import cv2 #opencv
 
+def suma(m1, m2):
+    m1 = cv2.resize(m1, (300,250))
+    m2 = cv2.resize(m2, (300,250))
+    mr = m1 + m2
+    cv2.imshow("suma",mr)
+    cv2.waitKey(0)
+    
+def suma2(m1, m2):
+    cv2.destroyAllWindows()
+    m1 = cv2.resize(m1, (300,250))
+    m2 = cv2.resize(m2, (300,250))
+    cv2.imshow('Imagen1', m1)
+    cv2.imshow('Imagen2', m2)    
+    mr = cv2.add( ima1,ima2)
+    cv2.imshow("suma2",mr)
+    cv2.waitKey(0)
+    
+def suma3(m1, m2):
+    cv2.destroyAllWindows()  
+    m1 = cv2.resize(m1, (300,250))
+    m2 = cv2.resize(m2, (300,250))
+    cv2.imshow('Imagen1', m1)
+    cv2.imshow('Imagen2', m2) 
+    mr = np.add( ima1,ima2)
+    cv2.imshow("suma3",mr)
+    cv2.waitKey(0)
+    
+def resta1(m1, m2):
+    cv2.destroyAllWindows()
+    m1 = cv2.resize(m1, (300,250))
+    m2 = cv2.resize(m2, (300,250))
+    cv2.imshow('Imagen1', m1)
+    cv2.imshow('Imagen2', m2)
+    mr = ima1-ima2
+    cv2.imshow("resta1",mr)
+    cv2.waitKey(0)
+    
+def resta2(m1, m2):
+    cv2.destroyAllWindows()
+    m1 = cv2.resize(m1, (300,250))
+    m2 = cv2.resize(m2, (300,250))
+    cv2.imshow('Imagen1', m1)
+    cv2.imshow('Imagen2', m2)
+    mr = np.subtract(ima1,ima2)
+    cv2.imshow("resta2",mr)
+    cv2.waitKey(0)
+    
+def resta3(m1, m2):
+    cv2.destroyAllWindows()
+    m1 = cv2.resize(m1, (300,250))
+    m2 = cv2.resize(m2, (300,250))
+    cv2.imshow('Imagen1', m1)
+    cv2.imshow('Imagen2', m2)
+    mr = cv2.subtract( ima1,ima2)
+    cv2.imshow("resta3",mr)
+    cv2.waitKey(0)
+    
+def division1(m1, m2):
+    cv2.destroyAllWindows()
+    m1 = cv2.resize(m1, (300,250))
+    m2 = cv2.resize(m2, (300,250))
+    cv2.imshow('Imagen1', m1)
+    cv2.imshow('Imagen2', m2)
+    mr = ima1/ima2
+    cv2.imshow("division1",mr)
+    cv2.waitKey(0)
+    
+def division2(m1, m2):
+    cv2.destroyAllWindows()
+    m1 = cv2.resize(m1, (300,250))
+    m2 = cv2.resize(m2, (300,250))
+    cv2.imshow('Imagen1', m1)
+    cv2.imshow('Imagen2', m2)
+    mr = np.divide(ima1,ima2)
+    cv2.imshow("division2",mr)
+    cv2.waitKey(0)
+    
+def division3(m1, m2):
+    cv2.destroyAllWindows()
+    m1 = cv2.resize(m1, (300,250))
+    m2 = cv2.resize(m2, (300,250))
+    cv2.imshow('Imagen1', m1)
+    cv2.imshow('Imagen2', m2)
+    mr = cv2.divide( ima1,ima2)
+    cv2.imshow("division3",mr)
+    cv2.waitKey(0)
+    
+def multiplicacion1(m1, m2):
+    cv2.destroyAllWindows()
+    m1 = cv2.resize(m1, (300,250))
+    m2 = cv2.resize(m2, (300,250))
+    cv2.imshow('Imagen1', m1)
+    cv2.imshow('Imagen2', m2)
+    mr = ima1*ima2
+    cv2.imshow("multiplicacion1",mr)
+    cv2.waitKey(0)
+    
+def multiplicacion2(m1, m2):
+    cv2.destroyAllWindows()
+    m1 = cv2.resize(m1, (300,250))
+    m2 = cv2.resize(m2, (300,250))
+    cv2.imshow('Imagen1', m1)
+    cv2.imshow('Imagen2', m2)
+    mr = np.multiply(ima1,ima2)
+    cv2.imshow("multiplicacion2",mr)
+    cv2.waitKey(0)
+    
+def multiplicacion3(m1, m2):
+    cv2.destroyAllWindows()
+    m1 = cv2.resize(m1, (300,250))
+    m2 = cv2.resize(m2, (300,250))
+    cv2.imshow('Imagen1', m1)
+    cv2.imshow('Imagen2', m2)
+    mr = cv2.multiply ( ima1,ima2)
+    cv2.imshow("multiplicacion3",mr)
+    cv2.waitKey(0)  
+    
+def raiz1(m1, m2):
+    cv2.destroyAllWindows()
+    m1 = cv2.resize(m1, (300,250))
+    m2 = cv2.resize(m2, (300,250))
+    cv2.imshow('Imagen1', m1)
+    cv2.imshow('Imagen2', m2)
+    mr = ima1**(0.5)
+    cv2.imshow("raiz1",mr)
+    cv2.waitKey(0) 
+    
+def raiz2(m1, m2):
+    cv2.destroyAllWindows()
+    m1 = cv2.resize(m1, (300,250))
+    m2 = cv2.resize(m2, (300,250))
+    cv2.imshow('Imagen1', m1)
+    cv2.imshow('Imagen2', m2)
+    mr = pow(ima1,0.5)
+    cv2.imshow("raiz2",mr)
+    cv2.waitKey(0)    
+    
+def raiz3(m1, m2):
+    cv2.destroyAllWindows()
+    m1 = cv2.resize(m1, (300,250))
+    m2 = cv2.resize(m2, (300,250))
+    cv2.imshow('Imagen1', m1)
+    cv2.imshow('Imagen2', m2)
+    mr = np.sqrt(ima1)
+    cv2.imshow("raiz3",mr)
+    cv2.waitKey(0) 
+    
+def derivada1(m1, m2):
+    cv2.destroyAllWindows()
+    m1 = cv2.resize(m1, (300,250))
+    m2 = cv2.resize(m2, (300,250))
+    cv2.imshow('Imagen1', m1)
+    cv2.imshow('Imagen2', m2)
+    dx = np.diff(ima1)
+    dy = np.diff(ima2)
+    d = dy/dx
+    cv2.imshow("derivada1",d)
+    cv2.waitKey(0)    
+    
+def potencia1(m1, m2):
+    cv2.destroyAllWindows()
+    m1 = cv2.resize(m1, (300,250))
+    m2 = cv2.resize(m2, (300,250))
+    cv2.imshow('Imagen1', m1)
+    cv2.imshow('Imagen2', m2)
+    mr = m1**2
+    cv2.imshow("potencia1",mr)
+    cv2.waitKey(0)    
+    
+def potencia2(m1, m2):
+    cv2.destroyAllWindows()
+    m1 = cv2.resize(m1, (300,250))
+    m2 = cv2.resize(m2, (300,250))
+    cv2.imshow('Imagen1', m1)
+    cv2.imshow('Imagen2', m2)
+    mr = np.power(m1,2)
+    cv2.imshow("potencia2",mr)
+    cv2.waitKey(0)    
+    
+def potencia3(m1, m2):
+    cv2.destroyAllWindows()
+    m1 = cv2.resize(m1, (300,250))
+    m2 = cv2.resize(m2, (300,250))
+    cv2.imshow('Imagen1', m1)
+    cv2.imshow('Imagen2', m2)
+    mr = cv2.pow(m1,2)
+    cv2.imshow("potencia3",mr)
+    cv2.waitKey(0)    
+    
+
 ima1 = cv2.imread('foto1.png')
 ima2 = cv2.imread('foto2.png')
 ima1 = cv2.resize(ima1, (300,250))
 ima2 = cv2.resize(ima2, (300,250))
 
-"""
-#suma metodo 1
 cv2.imshow('Imagen1', ima1)
 cv2.imshow('Imagen2', ima2)
-print("suma")
-suma = ima1+ima2
-print(suma)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+x=cv2.waitKey(0)
 
-#suma metodo 2
-cv2.imshow('Imagen1', ima1)
-cv2.imshow('Imagen2', ima2)
-print("adicion")
-adicion =cv2.add( ima1,ima2)
-print(adicion)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+while True:
 
-#suma metodo 3
-cv2.imshow('Imagen1', ima1)
-cv2.imshow('Imagen2', ima2)
-print("metodo 3")
-adicion =cv2.add( ima1,ima2)
-print(np.add(ima1,ima2))
-cv2.waitKey(0)
-cv2.destroyAllWindows()"""
-"""
-#resta metodo 1
-cv2.imshow('Imagen1', ima1)
-cv2.imshow('Imagen2', ima2)
-print("resta 1")
-resta = ima1-ima2
-print(resta)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    if x == ord("d"):
+        suma(ima1,ima2)
+        x=cv2.waitKey(0)
+        
+    if x == ord("d"):
+        suma2(ima1,ima2)
+        x=cv2.waitKey(0)
+        
+    if x == ord("d"):
+        suma3(ima1,ima2)
+        x=cv2.waitKey(0)
+        
+    if x == ord("d"):
+        resta1(ima1,ima2)
+        x=cv2.waitKey(0)
 
-#resta metodo 2
-cv2.imshow('Imagen1', ima1)
-cv2.imshow('Imagen2', ima2)
-print("metodo 2")
-print(np.subtract(ima1,ima2))
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+        
+    if x == ord("d"):
+        resta2(ima1,ima2)
+        x=cv2.waitKey(0)
+        
+    if x == ord("d"):
+        resta3(ima1,ima2)
+        x=cv2.waitKey(0)
+        
+    if x == ord("d"):
+        division1(ima1,ima2)
+        x=cv2.waitKey(0)
+        
+    if x == ord("d"):
+        division2(ima1,ima2)
+        x=cv2.waitKey(0)
+        
+    if x == ord("d"):
+        division3(ima1,ima2)
+        x=cv2.waitKey(0)
+        
+    if x == ord("d"):
+        multiplicacion1(ima1,ima2)
+        x=cv2.waitKey(0)
+        
+    if x == ord("d"):
+        multiplicacion2(ima1,ima2)
+        x=cv2.waitKey(0)
+        
+    if x == ord("d"):
+        multiplicacion3(ima1,ima2)
+        x=cv2.waitKey(0)         
+        
+    if x == ord("d"):
+        raiz1(ima1,ima2)
+        x=cv2.waitKey(0)  
+        
+    if x == ord("d"):
+        raiz2(ima1,ima2)
+        x=cv2.waitKey(0)     
+        
+    if x == ord("d"):
+        potencia1(ima1,ima2)
+        x=cv2.waitKey(0)        
+                      
+    if x == ord("d"):
+        potencia2(ima1,ima2)
+        x=cv2.waitKey(0)
+        
+    if x == ord("d"):
+        potencia3(ima1,ima2)
+        x=cv2.waitKey(0)        
+        
+        cv2.destroyAllWindows()        
+    break
 
-#resta metodo 3
-cv2.imshow('Imagen1', ima1)
-cv2.imshow('Imagen2', ima2)
-print("resta3")
-subtract =cv2.subtract( ima1,ima2)
-print(subtract)
-cv2.waitKey(0)
-cv2.destroyAllWindows()"""
-'''
-#division metodo 1
-cv2.imshow('Imagen1', ima1)
-cv2.imshow('Imagen2', ima2)
-print("division 1")
-division = ima1/ima2
-print(division)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
-#division metodo 2
-cv2.imshow('Imagen1', ima1)
-cv2.imshow('Imagen2', ima2)
-print("metodo 2")
-print(np.divide(ima1,ima2))
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
-#division metodo 3
-cv2.imshow('Imagen1', ima1)
-cv2.imshow('Imagen2', ima2)
-print("division 3")
-division =cv2.divide( ima1,ima2)
-print(division)
-cv2.waitKey(0)
-cv2.destroyAllWindows()'''
-'''
-#multiplicacion metodo 1
-cv2.imshow('Imagen1', ima1)
-cv2.imshow('Imagen2', ima2)
-print("multiplicacion 1")
-multiplicacion = ima1*ima2
-print(multiplicacion)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
-#multiplicacion metodo 2
-cv2.imshow('Imagen1', ima1)
-cv2.imshow('Imagen2', ima2)
-print('multiplicacion metodo 2')
-print(np.multiply(ima1,ima2))
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-#multiplicacion metodo 3
-cv2.imshow('Imagen1', ima1)
-cv2.imshow('Imagen2', ima2)
-print("multiplicacion 3")
-multiplicacion =cv2.multiply ( ima1,ima2)
-print(multiplicacion)
-cv2.waitKey(0)
-cv2.destroyAllWindows()'''
 '''
 #logn metodo 1
 cv2.imshow('Imagen1', ima1)
@@ -123,30 +289,12 @@ print(logn)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-'''
-#potencia metodo 1
-cv2.imshow('Imagen1', ima1)
-cv2.imshow('Imagen2', ima2)
-print("potencias 1")
-potencias = ima1**2
-print(potencias)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
-#potencia metodo 2
+#raiz meodo 3 
 cv2.imshow('Imagen1', ima1)
 cv2.imshow('Imagen2', ima2)
-print('potencia metodo 2')
-print(np.power(ima1,2))
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-#potencia metodo 3
-cv2.imshow('Imagen1', ima1)
-cv2.imshow('Imagen2', ima2)
-print("potencias 3")
-potencias = cv2.pow(ima1,2)
-print(potencias)
+print ('raiz metodo 3')
+print (np.sqrt(ima1))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
@@ -159,28 +307,4 @@ dy = np.diff(ima2)
 d = dy/dx
 print(d)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-#raiz metodo 1
-cv2.imshow('Imagen1', ima1)
-cv2.imshow('Imagen2', ima2)
-print ('raiz metodo 1')
-print (ima1**(0.5))
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-#raiz meodo 2 
-cv2.imshow('Imagen1', ima1)
-cv2.imshow('Imagen2', ima2)
-print ('raiz metodo 2')
-print (pow(ima1,0.5))
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-#raiz meodo 3 
-cv2.imshow('Imagen1', ima1)
-cv2.imshow('Imagen2', ima2)
-print ('raiz metodo 3')
-print (np.sqrt(ima1))
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.destroyAllWindows()'''
